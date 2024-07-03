bf31167c6c2cede3d861cb2d8ed9b13b6f61bcad
Blackeye0011 <comrade.78926@gmail.com>
2024-07-03 22:30:03 +0530

    HelloWorld application

---
+++ app/src/main/java/com/example/a1stpro/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a1stpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b;
+        b = findViewById(R.id.hello);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Remove the unnecessary 'Object Application' declaration
+                Toast.makeText(MainActivity.this, "Hey! We are using Android Application", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
